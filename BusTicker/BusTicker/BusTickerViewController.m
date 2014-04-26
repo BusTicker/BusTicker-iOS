c3fd7fc465644292139b3cef2f6b56de5fbf6d0c
Jake Widmer <jwidmer@tsrsolutions.com>
2014-04-26 14:42:39 -0500

    Added basic main view

---
+++ BusTicker/BusTicker/BusTickerViewController.m
@@ -0,0 +1,49 @@
+//
+//  ViewController.m
+//  BusTicker
+//
+//  Created by Jake Widmer on 4/26/14.
+//  Copyright (c) 2014 HardwareHackMKE. All rights reserved.
+//
+
+#import "BusTickerViewController.h"
+
+@interface BusTickerViewController ()
+
+@property (strong, nonatomic) IBOutlet UILabel *firstTimeLabel;
+@property (strong, nonatomic) IBOutlet UILabel *secondTimeLabel;
+
+@property (strong, nonatomic) IBOutlet UILabel *routeNameLabel;
+
+@end
+
+@implementation BusTickerViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    [self setNeedsStatusBarAppearanceUpdate];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (UIStatusBarStyle)preferredStatusBarStyle{
+    return UIStatusBarStyleLightContent;
+}
+
+#pragma mark - Action handlers
+- (IBAction)favoritesButtonPressed:(id)sender {
+    
+}
+
+- (IBAction)mapButtonPressed:(id)sender {
+    
+}
+
+
+@end
