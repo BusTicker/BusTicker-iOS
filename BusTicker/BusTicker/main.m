97c3c077479521c8bd4003222a5d1d46a091146b
Brennan Stehling <brennan@smallsharptools.com>
2014-09-20 12:13:06 -0500

    recreated project for a fresh start with Xcode 6

--- BusTicker/BusTicker/main.m
+++ BusTicker/BusTicker/main.m
@@ -2,16 +2,14 @@
 //  main.m
 //  BusTicker
 //
-//  Created by Jake Widmer on 4/26/14.
-//  Copyright (c) 2014 HardwareHackMKE. All rights reserved.
+//  Created by Brennan Stehling on 9/20/14.
+//  Copyright (c) 2014 BusTicker. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
-
 #import "AppDelegate.h"
 
-int main(int argc, char * argv[])
-{
+int main(int argc, char * argv[]) {
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
