d7948762ac65dd189217f82382281ecf7c2dba55
Brennan Stehling <brennan@smallsharptools.com>
2014-09-27 16:22:59 -0500

    minor adjustments

--- BusTicker/BusTicker/PulsingView.h
+++ BusTicker/BusTicker/PulsingView.h
@@ -8,19 +8,24 @@
 
 #import <UIKit/UIKit.h>
 
+//#if !TARGET_INTERFACE_BUILDER
+//#else
+//#endif
+
+IB_DESIGNABLE
 @interface PulsingView : UIView
 
-@property (nonatomic, strong) UIColor *innerColor; // default white
-@property (nonatomic, strong) UIColor *outerColor; // default darkGray
+@property (nonatomic, strong) IBInspectable UIColor *innerColor; // default white
+@property (nonatomic, strong) IBInspectable UIColor *outerColor; // default darkGray
 
-@property (nonatomic, readwrite) CGFloat innerDiameter; // default bounds width
-@property (nonatomic, readwrite) CGFloat outerDiameter; // default bounds width / 4
+@property (nonatomic, readwrite) IBInspectable CGFloat innerDiameter; // default bounds width
+@property (nonatomic, readwrite) IBInspectable CGFloat outerDiameter; // default bounds width / 4
 
-@property (nonatomic, readwrite) NSTimeInterval innerDuration; // default 2s
-@property (nonatomic, readwrite) NSTimeInterval outerDuration; // default 3s
+@property (nonatomic, readwrite) IBInspectable NSUInteger innerDuration; // default 2s
+@property (nonatomic, readwrite) IBInspectable NSUInteger innerDelay; // default 1s
 
-@property (nonatomic, readwrite) NSTimeInterval innerDelay; // default 1s
-@property (nonatomic, readwrite) NSTimeInterval outerDelay; // default 2s
+@property (nonatomic, readwrite) IBInspectable NSUInteger outerDuration; // default 3s
+@property (nonatomic, readwrite) IBInspectable NSUInteger outerDelay; // default 2s
 
 - (void)start;
 - (void)stop;
